02115a188aa01184d96d27968c35526902c94510
tachyla <tachylad.murray@gmail.com>
2017-04-07 12:39:53 -0700

    created event listner for shopping list

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 //state object
 const appState = {
+	items: []
+};
+
+const testState = {
 	items: [
 		{ title: 'apples', done: true },
 		{ title: 'oranges', done: true },
@@ -13,44 +17,63 @@ const appState = {
 //function to add item
 function addItem(appState, item) {
 	appState.items.push(item);
-}
+};
 
 
 //function to remove item
-function removeItem(appState, itemIndex) {
-	appState.items.splice(itemIndex, 1);
-}
+// function removeItem(appState, itemIndex) {
+// 	appState.items.splice(itemIndex, 1);
+// }
 
 //function to check item, no jq - toggle boolean in appState
 // instead .shopping-item__checked 
-function checkItem(appState, item) {
-	let checkedItem = appState.item.strike();
-}
+// function checkItem(appState, item) {
+// }
 
 
 //render functions
 function renderList(appState, element) {
-	let itemsHTML = appState.items.map(function) {
-		return `<li> item </li>`;
-	}
+	let itemsHTML = appState.items.map(function(item) {
+		return `<li>
+        			<span class="shopping-item">${item}</span>
+        			<div class="shopping-item-controls">
+          				<button class="shopping-item-toggle">
+            				<span class="button-label">check</span>
+          				</button>
+          				<button class="shopping-item-delete">
+            				<span class="button-label">delete</span>
+          				</button>
+        			</div>
+      			</li>`;
+	});
 	element.html(itemsHTML);
-}
-
+};
 
 
 //event listeners, calll after 
-function addEventListeners() {
-	$('.shopping-list-add').submit(function(event) {
-		event.preventDefault();
-    	let item = $('.shopping-list-add-input').val();
-    	addItem(appState, item);
+$(document).ready(function (addEventListeners) {
+	$('.shopping-list-add').on("click",function(event) {
 
+    	let item = addItem(appState, $('.shopping-list-add-input').val());
+    	//let item = $('.shopping-list').append('<li>');
+    	//$('.shopping-list').append
     	renderList(appState, $('.shopping-list'));
 	});
-}
+	// $('.shopping-item-delete').remove(function(event) {
+	// 	event.preventDefault();
+ //    	let itemToRemove = $('.shopping-list-remove-input').val();
+ //    	removedItem(appState, item);
+ //    	renderList(appState, $('.shopping-list'));
+	// });
+	// $('.shopping-item-toggle').check(function(event) {
+	// 	event.preventDefault();
+ //    	let item = $('.shopping-list-toggle-input').val();
+ //    	addItem(appState, item);
+ //    	renderList(appState, $('.shopping-list'));
+	// });
+});
 
 console.log(appState);
-console.log("hello world");
 
 
 
